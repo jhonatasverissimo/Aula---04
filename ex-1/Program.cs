232df872fc25b86796cc126cda2e6e1d61c726d4
jhonny <jhonny-9812@hotmail.com>
2022-09-02 10:49:14 -0300

    V01 - Tarefas Aula 04

---
+++ ex-1/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+double raio, area, diametro, perimetro;
+
+Console.Write("Informe a o raio do Circulo: ");
+raio = Convert.ToDouble(Console.ReadLine());
+
+area = 3.14 * (raio*raio);
+diametro = 2*raio;
+perimetro = 2*3.14*raio; 
+
+Console.WriteLine("A area do circulo é:" + area);
+Console.WriteLine("O diametro do circulo é:" + diametro);
+Console.WriteLine("O perimetro do circulo é:" + perimetro);
+
---
+++ ex-2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int a,b,c;
+double delta, x1, x2;
+
+Console.Write("Entr com o valor de A: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Entr com o valor de B: ");
+b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Entr com o valor de C: ");
+c = Convert.ToInt32(Console.ReadLine());
+
+delta = (b*b)-4*a*c;
+
+x1 = (-b + Math.Sqrt(delta)) / (2*a);
+x2 = (+b + Math.Sqrt(delta)) / (2*a);
+
+Console.WriteLine(delta);
+Console.WriteLine("X1 = " + x1);
+Console.WriteLine("X2 = " + x2);
+
+
+
