dd9b7543b5413d29d2866fcba4436343d9e416bc
jhonny <jhonny-9812@hotmail.com>
2022-09-03 14:21:25 -0300

    V02 - Correção da fórmula

--- ex-2/Program.cs
+++ ex-2/Program.cs
@@ -14,7 +14,7 @@ c = Convert.ToInt32(Console.ReadLine());
 delta = (b*b)-4*a*c;
 
 x1 = (-b + Math.Sqrt(delta)) / (2*a);
-x2 = (+b + Math.Sqrt(delta)) / (2*a);
+x2 = (-b - Math.Sqrt(delta)) / (2*a);
 
 Console.WriteLine(delta);
 Console.WriteLine("X1 = " + x1);
